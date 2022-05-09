b908297fa474587a5e791cb015fd2bda6332cec3
Krishan Gharu <kslg257@gmail.com>
2022-05-09 20:34:46 +0000

    Created get order data function to get order details from the user

--- run.py
+++ run.py
@@ -13,8 +13,32 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('wired_coffee_branch_orders')
 
-orders = SHEET.worksheet('orders')
+def get_order_data():
+    """
+    Get order data input from the user
+    """
+    print("Welcome to the Wired Coffee B2B Ordering Plaform.\n")
+    print("You can order one product at a time.\n")
+    print(" **** Your Top 5 Products to Order: ****\n")
+    print("Coffee Cup\n")
+    print("Sip Lids\n")
+    print("Coffee Beans\n")
+    print("Suger Sticks\n")
+    print("Drinks Stirrer\n")
+    print("==============================\n")
+    print("Please enter order details here:\n")
+    user_name = input("Your Full Name:")
+    branch_number = input("Enter your Branch Number:")
+    product_sku = input("Enter the Product SKU:")
+    product_qty = input("Enter the quantity:")
+    payment_method = input("How would you like to pay? Choose either Bank Transfer or Pay on Account:")
+    confirm_order = input()
+    print("**** Order Preview: ****\n")
+    print(f"Order Raised by {user_name}")
+    print(f"Branch Number is {branch_number}")
+    print(f"Product Name {product_sku}")
+    print(f"Quantity {product_qty}")
+    print(f"Payment Method {payment_method}")
+    print(f"Confirm Order? {confirm_order}")
 
-data = orders.get_all_values()
-
-print(data)
+get_order_data()
